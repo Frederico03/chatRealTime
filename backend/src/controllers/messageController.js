ec0e4fa69e660ac27ebf1b63434b1c2a7c7a950a
FredericoGarcez <frederico.rodrigues@discente.ufg.br>
2024-01-17 00:02:07 -0300

    Update Send Messages

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -17,4 +17,23 @@ export const addMessage = async (req, res, next) => {
 }
 
 export const getAllMessage = async (req, res, next) => {
+  try {
+    const {from, to} = req.body;
+    const messages = await messageModel
+      .find({
+        users: {
+          $all: [from, to],
+        }
+      })
+      .sort({updateAt: 1})
+      const projectMessages = messages.map((msg) => {
+        return {
+          fromSelf: msg.sender.toString() === from,
+          message: msg.message.text,
+        };
+      });
+      res.json(projectMessages);
+  } catch (error) {
+    next(error)
+  }
 }
